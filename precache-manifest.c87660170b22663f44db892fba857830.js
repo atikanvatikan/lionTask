5937b237c16bffad9eb52fc9ac3aedfce35eedb2
atikanvatikan <atakantemel92@gmail.com>
2019-05-28 21:36:06 +0200

    Updates

---
+++ precache-manifest.c87660170b22663f44db892fba857830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0874c37f6d7e201e45c458d6e5f3bd",
+    "url": "/lionTask/index.html"
+  },
+  {
+    "revision": "c9e8d9ad26446c85c5d7",
+    "url": "/lionTask/static/css/main.635bc61e.chunk.css"
+  },
+  {
+    "revision": "d79e81dacc44e4b5b4aa",
+    "url": "/lionTask/static/js/2.ec987513.chunk.js"
+  },
+  {
+    "revision": "c9e8d9ad26446c85c5d7",
+    "url": "/lionTask/static/js/main.4c207ed2.chunk.js"
+  },
+  {
+    "revision": "00867f73ff4d0868531d",
+    "url": "/lionTask/static/js/runtime~main.1aaf5789.js"
+  }
+]);
